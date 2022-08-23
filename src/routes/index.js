a749cddcb970bbb27ae4a43389be02391ac00e77
joabesimao <simaojoabecosta@gmail.com>
2022-08-23 18:42:53 -0300

    cria rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,7 @@
 const bodyParser = require("body-parser");
+const pessoas = require("./pessoasRoute");
 
-module.exports = (app)=>{
-    app.use(
-        bodyParser.json(),
-        bodyParser.urlencoded({extended:false}),
-    )
-}
+module.exports = (app) => {
+  app.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }));
+  app.use(pessoas);
+};
