11610e02c6d8a8553ca7a475d7d6354213752d49
joabesimao <simaojoabecosta@gmail.com>
2022-08-23 18:42:02 -0300

    atualiza o index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
-
 const app = express();
-const routes = require("./routes")
+const routes = require("./routes");
 const port = 3000;
 
-routes(app)
+routes(app);
 
-app.listen(port, () => console.log(`listen on ${port}`));
+
+
+app.listen(port, () => console.log(`listen on port ${port}`));
 
 module.exports = app;
